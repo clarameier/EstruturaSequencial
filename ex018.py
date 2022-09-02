349126d6db6d76ece06a1e8fb37c2493acfcf97d
Maria Clara Rosa Meier <105031629+clarameier@users.noreply.github.com>
2022-09-02 17:12:10 -0300

    Update ex018.py
    subir alterações

--- ex018.py
+++ ex018.py
@@ -1,10 +1,8 @@
 # Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
 
-import math
+mb = float(input('Tamanho do arquivo, em MB: '))
+mbps = float(input('Velocidade do link da sua internet, em Mbps: '))
+seg = mb / mbps
+min = seg / 60
 
-arquivo = float(input('Tamanho do arquivo (MB):'))
-velocidade = float(input('Velocidade do link de Internet (Mbps): '))
-velocidade_MBs = velocidade/8
-segundos = arquivo/velocidade_MBs
-
-print('Tempo aproximado de download: ', segundos//60, 'min', math.ceil(segundos%60), 's')
+print('Seu arquivo levará {:.2f} minutos para carregar!'.format(min))
